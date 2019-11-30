9254bc7d812b58e152757d8c6428bc2498ad13fc
LordMomo <henriquetortura.ht@gmail.com>
2019-11-30 15:11:36 -0300

    RLC Simples com alpha, omega e fator entre eles.

--- projeto5.py
+++ projeto5.py
@@ -6,14 +6,17 @@ import matplotlib.pyplot as plt
 
 #%% Variáveis
 
-def F():#Fonte/L com auto-indutância L = 1
-    return 10**7
-    
-def alpha(): #R/2L
-    return 2.5*10**10
+def F():#Fonte
+    return 10**7 
 
-def omega(): #(LC)^(-1/2) com capacitância C = 10^(-16)
+def Factor():
+    return 0.25
+
+def omega():
     return 10**8
+
+def alpha():
+    return omega()*Factor()
 
 '''
 Escolhendo:
@@ -28,16 +31,34 @@ Escolhendo:
     alpha < omega
 
 '''
-
 #%% 
 
 def RLC(Vx, t):
     V,x = Vx
-    res = np.array([x,(F() - ((omega())**2)*V - 2*alpha()*x)])
+    res = np.array([x,(-2*alpha()*x - omega()**2*V + F())])
     return res
     
 t = np.linspace(0.0,0.6e-6,1001)    
 v,x = odeint(RLC,[0.0,0.0],t).T
 
-plt.plot(t,v)
+#%% Plot Charge
+
+fontsize = 16
+fig, ax = plt.subplots(figsize=(8, 4))
+ax.set_xlabel('Tempo', fontsize = fontsize)
+ax.plot(t,x)
+#ax.legend(loc = 'upper left')
+ax.grid(which='both')
+
+plt.savefig('RLC_Simples.png',dpi=800,bbox_inches='tight')
+plt.show()
+
+#%% Plot Current
+fig, ax = plt.subplots(figsize=(8, 4))
+ax.set_xlabel('Tempo', fontsize = fontsize)
+ax.plot(t,v)
+#ax.legend(loc = 'upper left')
+ax.grid(which='both')
+
+plt.savefig('RLC_Simples.png',dpi=800,bbox_inches='tight')
 plt.show()
