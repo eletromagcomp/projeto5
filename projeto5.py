48a7a8831e7865827974f3b5edf13be0f342dff9
bidufranjoao <49568329+bidufranjoao@users.noreply.github.com>
2019-11-30 15:04:16 -0300

    Update projeto5.py

--- projeto5.py
+++ projeto5.py
@@ -1,7 +1,3 @@
-print("Hello World")
-
-# Para um sistema RLC simples em série:
-
 #%% Importando
 import numpy as np
 from scipy.integrate import odeint
@@ -10,24 +6,34 @@ import matplotlib.pyplot as plt
 
 #%% Variáveis
 
-def F():#Fonte
-    return int(1) 
+def F():#Fonte/L com auto-indutância L = 1
+    return 10**7
+    
+def alpha(): #R/2L
+    return 2.5*10**10
 
-def RC():
-    R = 5.0
-    C = 1.0e-9
-    return R*C
+def omega(): #(LC)^(-1/2) com capacitância C = 10^(-16)
+    return 10**8
 
-def LC():
-    L = 100.0e-9
-    C = 1.0e-9
-    return L*C
+'''
+Escolhendo:
+    
+    Underdamped
+    alpha > omega
+    
+    Critically damped
+    alpha = omega
+    
+    Overdamped
+    alpha < omega
+
+'''
 
 #%% 
 
 def RLC(Vx, t):
     V,x = Vx
-    res = np.array([x,(F() - V - RC()*x)/(LC())])
+    res = np.array([x,(F() - ((omega())**2)*V - 2*alpha()*x)])
     return res
     
 t = np.linspace(0.0,0.6e-6,1001)    
