9a875e95c34a9ffdc3066ca1997cb69703e160ed
Henrique Tórtura <henriquetortura.ht@gmail.com>
2019-12-01 22:04:37 -0300

    Desnecessário com novo projeto5_debug

--- projeto5_gif.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import util5 as u5
-import cProfile
-
-cProfile.run('RLC = u5.RLC_Simples()')
-
-cProfile.run('factors = np.linspace(0.25, 1.75, 20)')
-
-cProfile.run('u5.Create_GIF(RLC, factors)')
